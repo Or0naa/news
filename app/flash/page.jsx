a24e23af62e14fc93b50d6ccf1b4406cdcb6091b
eliraz <elish7@gmail.com>
2024-05-19 15:13:32 +0300

    little cahnges

--- app/flash/page.jsx
+++ app/flash/page.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { getAllFlashes } from '@/server/BL/flash.service'
+import { connectToMongo } from '@/server/DL/connectToMongo'
 
 export default function AllFlash() {
+  connectToMongo()
+const flashes = getAllFlashes()
+
   return (
-    <div>page</div>
+    <div>
+      {Object.keys(flashes).map(flash => 
+      <div className='text-red-200'>
+        {flash.title}
+        </div>)}
+    </div>
   )
 }
