9589b9ca8149d5169df3603a657e9516aab85445
eliraz <elish7@gmail.com>
2024-05-22 16:58:42 +0300

    flsh fix

--- components/FlashNav.jsx
+++ components/FlashNav.jsx
@@ -1,6 +1,6 @@
-"use client"
 import { getAllFlashes } from '@/server/BL/flash.service';
 import { connectToMongo } from '@/server/DL/connectToMongo';
+import FlashNavContent from './FlashNavContent';
 
 export default async function FlashNav() {
   await connectToMongo();
@@ -12,57 +12,6 @@ export default async function FlashNav() {
   }
 
   return (
-    <>
-      <div id="flash-container"
-       className="text-theme-accent min-h-36 flex flex-col gap-1 p-2 items-center bg-theme-light rounded-lg border-2 border-theme-dark">
-        {flashes.slice(0, 4).map((flash) => (
-          <div key={flash.id} className="flash-item w-full flex flex-col items-center gap-1">
-            {flash.title}
-          </div>
-        ))}
-      </div>
-      <style jsx>{`
-        @keyframes fadeInUp {
-          from {
-            opacity: 0;
-            transform: translateY(20px);
-          }
-          to {
-            opacity: 1;
-            transform: translateY(0);
-          }
-        }
-
-        .fade-in-up {
-          animation: fadeInUp 0.5s ease-in-out;
-        }
-      `}</style>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-      (function() {
-        const flashes = ${JSON.stringify(flashes)};
-        let index = 0;
-        const intervalId = setInterval(() => {
-          const flashContainer = document.getElementById('flash-container');
-          if (flashContainer) {
-            index += 1;
-            if (index >= flashes.length) {
-              index = 0;
-            }
-            const newFlashes = flashes.slice(index, index + 4);
-            flashContainer.innerHTML = newFlashes.map(flash => \`
-              <div key=\${flash.id}
-               class="flash-item fade_in_up w-full flex flex-col items-center gap-1">
-                \${flash.title}
-              </div>
-            \`).join('');
-          }
-        }, 3000);
-      })();
-    `,
-        }}
-      />
-    </>
+   <FlashNavContent flashes={flashes} />
   );
 }
--- server/DL/controllers/article.controller.js
+++ server/DL/controllers/article.controller.js
@@ -1,8 +1,8 @@
 
 import { ArticleModel } from "../article.model";
-import { getAllArticles, createArticleServer, updateArticle } from "@/server/BL/article.service";
+import { getAllArticles, createArticleService, updateArticle } from "@/server/BL/article.service";
 
 export const getArticles =  () => getAllArticles();
 export const getArticleById =  (id) =>  ArticleModel.findById(id)
-export const createNewArticle =  (article) =>  createArticleServer(article)
+export const createNewArticle =  (article) =>  createArticleService(article)
 export const updateArticleById =  (id, article) =>  updateArticle(id, article)
