fa42dd4b728e4738fc0b7f29f8d8b42442bea1eb
eliraz <elish7@gmail.com>
2024-05-22 10:21:01 +0300

    little server changes

--- server/DL/controllers/article.controller.js
+++ server/DL/controllers/article.controller.js
@@ -1,7 +1,8 @@
 
 import { ArticleModel } from "../article.model";
-import { getAllArticles, createArticle } from "@/server/BL/article.service";
+import { getAllArticles, createArticleServer, updateArticle } from "@/server/BL/article.service";
 
 export const getArticles =  () => getAllArticles();
 export const getArticleById =  (id) =>  ArticleModel.findById(id)
 export const createNewArticle =  (article) =>  createArticleServer(article)
+export const updateArticleById =  (id, article) =>  updateArticle(id, article)
