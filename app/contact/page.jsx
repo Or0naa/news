96d39e97881d667050e52e4054802c506d111c61
eliraz <elish7@gmail.com>
2024-05-22 18:17:11 +0300

    little changes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,17 +1,19 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
+import WhatsAppButtonGreenSmall from '/public/WhatsAppButtonGreenSmall.png'
 
 export default function Contact() {
     return (
         <div className="flex flex-col items-center gap-4">
-            <Link href="tel:+9720542495211">📲 לטלפון לאור-נא:</Link>
-            <Link href="mailto:or0naa@gmail.com">📧 לחץ לשליחת אימייל לאור-נא</Link>
-            <a aria-label="Chat on WhatsApp" href="https://wa.me/0542495211"><img alt="Chat on WhatsApp" src="WhatsAppButtonGreenLarge.svg" />‎
-‎‏</a >
-            <Link href="tel:+9720503210090">📲 לטלפון לאלירז:</Link>
-            <Link href="mailto:elish7@gmail.com">📧 לחץ לשליחת אימייל לאלירז</Link>
-            <a aria-label="Chat on WhatsApp" href="https://wa.me/0503210090"><img alt="Chat on WhatsApp" src="WhatsAppButtonGreenLarge.svg" />‎
-‎‏</a >
+            <Link href="tel:+9720542495211" target='_blank'>📲 לטלפון לאור-נא:</Link>
+            <Link href="mailto:or0naa@gmail.com" target='_blank'>📧 לחץ לשליחת אימייל לאור-נא</Link>
+            <Link href="https://wa.me/0542495211" target='_blank'>  <Image width='15px' height='10px' alt=''  src={WhatsAppButtonGreenSmall}  >
+                </Image ></Link>  
+            <Link href="tel:+9720503210090" target='_blank'>📲 לטלפון לאלירז:</Link>
+            <Link href="mailto:elish7@gmail.com" target='_blank'>📧 לחץ לשליחת אימייל לאלירז</Link>
+      <Link   href="https://wa.me/0503210090" target='_blank'>   <Image width='15px' height='10px' alt='' src={WhatsAppButtonGreenSmall}  >
+                </Image ></Link>
 
         </div>
     )
--- components/FlashNavContent.jsx
+++ components/FlashNavContent.jsx
@@ -1,59 +1,47 @@
-"use client"
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react';
 
 export default function FlashNavContent({ flashes }) {
-  return (
-    <>
-     <div id="flash-container" key={flashes.id}
-    className="text-theme-accent min-h-36 flex flex-col gap-1 p-2 items-center bg-theme-light rounded-lg border-2 border-theme-dark">
-     {flashes.slice(0, 4).map((flash) => (
-       <div key={flash.id} className="flash-item w-full flex flex-col items-center gap-1">
-         {flash.title}
-       </div>
-     ))}
-   </div>
-   <style jsx>{`
-     @keyframes fadeInUp {
-       from {
-         opacity: 0;
-         transform: translateY(20px);
-       }
-       to {
-         opacity: 1;
-         transform: translateY(0);
-       }
-     }
+  const [currentFlashes, setCurrentFlashes] = useState(flashes.slice(0, 4));
+  const [index, setIndex] = useState(0);
 
-     .fade-in-up {
-       animation: fadeInUp 0.5s ease-in-out;
-     }
-   `}</style>
-   <script
-     dangerouslySetInnerHTML={{
-       __html: `
-   (function() {
-     const flashes = ${JSON.stringify(flashes)};
-     let index = 0;
-     const intervalId = setInterval(() => {
-       const flashContainer = document.getElementById('flash-container');
-       if (flashContainer) {
-         index += 1;
-         if (index >= flashes.length) {
-           index = 0;
-         }
-         const newFlashes = flashes.slice(index, index + 4);
-         flashContainer.innerHTML = newFlashes.map(flash => \`
-           <div key=\${flash.id}
-            class="flash-item fade_in_up w-full flex flex-col items-center gap-1">
-             \${flash.title}
-           </div>
-         \`).join('');
-       }
-     }, 3000);
-   })();
- `,
-     }}
-   />
- </>
-  )
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIndex((prevIndex) => {
+        const newIndex = (prevIndex + 1) % flashes.length;
+        setCurrentFlashes(flashes.slice(newIndex, newIndex + 4));
+        return newIndex;
+      });
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [flashes]);
+
+  return (
+    <div>
+      <div id="flash-container" className="text-theme-accent min-h-36 flex flex-col gap-1 p-2 items-center bg-theme-light rounded-lg border-2 border-theme-dark">
+        {currentFlashes.map((flash) => (
+          <div key={flash.id} className="flash-item fade-in-up w-full flex flex-col items-center gap-1">
+            {flash.title}
+          </div>
+        ))}
+      </div>
+      <style jsx>{`
+        @keyframes fadeInUp {
+          from {
+            opacity: 0;
+            transform: translateY(20px);
+          }
+          to {
+            opacity: 1;
+            transform: translateY(0);
+          }
+        }
+
+        .fade-in-up {
+          animation: fadeInUp 0.5s ease-in-out;
+        }
+      `}</style>
+    </div>
+  );
 }
