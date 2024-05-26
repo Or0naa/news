a8a2b13619dd14b0967ba082e872d431d726f582
eliraz <elish7@gmail.com>
2024-05-26 19:39:51 +0300

    flash changes

--- components/FlashNavContent.jsx
+++ components/FlashNavContent.jsx
@@ -8,7 +8,7 @@ export default function FlashNavContent({ flashes }) {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setIndex((prevIndex) => {
-        const newIndex = (prevIndex + 1) % flashes.length;
+        const newIndex = (prevIndex + 4) % flashes.length;
         setCurrentFlashes(flashes.slice(newIndex, newIndex + 4));
         return newIndex;
       });
@@ -19,9 +19,10 @@ export default function FlashNavContent({ flashes }) {
 
   return (
     <div>
-      <div id="flash-container" className="text-theme-accent min-h-36 flex flex-col gap-1 p-2 items-center bg-theme-light rounded-lg border-2 border-theme-dark">
+      <h1 className='flex justify-center items-center mb-2 text-2xl'>מבזקים</h1>
+      <div id="flash-container" className="text-theme-accent min-h-screen flex flex-col gap-2 p-3 items-center">
         {currentFlashes.map((flash) => (
-          <div key={flash.id} className="flash-item fade-in-up w-full flex flex-col items-center gap-1">
+          <div key={flash.id} dir='auto' className="flash-item fade-in-up w-full flex flex-col items-center justify-center p-8 border-4 bg-theme-light border-theme-dark" style={{ height: '25vh' }}>
             {flash.title}
           </div>
         ))}
@@ -40,6 +41,13 @@ export default function FlashNavContent({ flashes }) {
 
         .fade-in-up {
           animation: fadeInUp 0.5s ease-in-out;
+        }
+
+        .flash-item {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          border-radius: 8px;
         }
       `}</style>
     </div>
