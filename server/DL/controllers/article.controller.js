94bf08f36d75095996ff5237fa59c509ab8205af
eliraz <elish7@gmail.com>
2024-05-26 19:03:33 +0300

    playing with admin

--- server/DL/controllers/article.controller.js
+++ server/DL/controllers/article.controller.js
@@ -1,10 +1,6 @@
 
 import { ArticleModel } from "../article.model";
-<<<<<<< HEAD
-import { getAllArticles, createArticleServer, updateArticle, getArticles } from "@/server/BL/article.service";
-=======
 import { getAllArticles, createArticleService, updateArticle } from "@/server/BL/article.service";
->>>>>>> d8003fa73996174e74a8554b367feebcf0d5b548
 
 // export const getArticles =  (filter={}) => getAllArticles();
 export const searchArticles = async (query) => await getArticles({ $text: { $search: query } });
